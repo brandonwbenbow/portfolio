709c82fb3f2ad6e642779f2df1e274d617de98b3
Brandon Benbow <brandon@katelyns-mbp.home>
2020-12-24 21:20:50 -0600

    Fixing Github Authentication

--- src/pages/devlog.js
+++ src/pages/devlog.js
@@ -16,12 +16,14 @@ class Blog extends React.Component {
     const posts = data.allMdx.edges
     const localSearchBlog = data.localSearchBlog
 
+    console.log(posts);
+
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         <div style={{width: "100%", height: "100vh", backgroundColor: "#333333", display: "flex", flexDirection: "column", alignItems: "center"}}>
           <div style={{widht: "100%", display: "flex", justifyContent: "center", alignItems: "center", height: "10vh", width: "70%", flexWrap: "wrap", marginTop: "15px"}}>
-            <h1 style={{color: "#eaeaea", margin: "0px", flex: 1}}>Blog</h1>
+            <h1 style={{color: "#eaeaea", margin: "0px", flex: 1}}>Devlog</h1>
             <div style={{alignItems: "center", flex: 1, display: "flex", justifyContent: "space-between"}}>
               <StyledLink className="fit-blog" path="/" extraStyle={{padding: "5px", marginLeft: "10px", marginRight: "10px"}}>
                 home
