81a31291fc0a1ac621bb6ece5252be413cf9d480
Brandon Benbow <brandonwbenbow@gmail.com>
2024-01-07 22:33:43 -0600

    Added More to Idle Game, Optimized Save File

--- public/js/idle.js
+++ public/js/idle.js
@@ -1,12 +1,13 @@
 import { accurateTimer } from "./time.js";
 
 class IdleGame {
+    all_time = new Produce();
     stockpile = new Produce();
     growth = new Produce();
 
-    resources = [];
-    buildings = [];
-    producers = [];
+    resources = new Map();
+    buildings = new Map();
+    producers = new Map();
 
     constructor() {
         this.ConstructDom();
@@ -36,9 +37,15 @@ class IdleGame {
     Tick() {
         this._last_tick_time = Date.now();
 
-        for(let i = 0; i < this.producers.length; i++) {
-            this.stockpile.add(this.producers[i].produce);
+        let tick_produce = new Produce();
+        let p = Array.from(this.producers, ([name, value]) => value);
+        for(let i = 0; i < p.length; i++) {
+            // this.stockpile.add(this.producers[i].produce);
+            tick_produce.add(p[i].asCount());
         }
+
+        // Add to Game Produce Counts
+        this.AddToPile(tick_produce);
 
         // Save to LocalStorage
         this.Save();
@@ -47,24 +54,52 @@ class IdleGame {
         // cancel tick with this._timer.cancel()
     }
 
+    AddToPile(produce) {
+        this.growth = produce;
+        this.stockpile.add(produce);
+        this.all_time.add(produce);
+    }
+
+    AddProducer(producer) {
+        if(this.producers.has(producer.name)) { return this.producers.get(producer.name).count += 1; }
+        this.producers.set(producer.name, producer);
+    }
+
     ConstructDom() {
-        const createButton = (text, callback) => {
+        const createButton = (text, title, callback, cost) => {
             let elem = document.createElement('button');
             elem.textContent = text;
-            elem.onclick = callback;
+            elem.setAttribute('title', title);
+            elem.onclick = cost ? () => {
+                if(!this.stockpile.has(cost)) { return; }
+                this.stockpile.remove(cost); callback();
+            } : callback;
             return elem;
         }
 
         let group = document.getElementById('buttons');
-        group.appendChild(createButton("Tree", () => { 
-            this.stockpile.work += 2; this.stockpile.food += 1; 
-        }));
 
-        group.appendChild(createButton("Wood Cutter", () => {
-            if(this.stockpile.food < 10) { return; }
-            this.stockpile.food -= 10;
-            this.producers.push(new Producer("Wood Cutter", new Produce(0.2, 0.4)));
-        }));
+        // Trees
+        group.appendChild(createButton("Tree", "1 Food, 2 Work", () => { this.AddToPile(new Produce(1, 2)); }));
+        group.appendChild(createButton("Wood Cutter", "0.2 Food, 0.4 Work per Tick", () => {
+            this.AddProducer(new Producer("Wood Cutter", new Produce(0.2, 0.4)));
+        }, new Produce(10)));
+        
+        group.appendChild(document.createElement('br'));
+
+        // Rock
+        group.appendChild(createButton("Rock", "4 Work", () => { this.AddToPile(new Produce(0, 4)); }));
+        group.appendChild(createButton("Stone Cutter", "1 Work per Tick", () => {
+            this.AddProducer(new Producer("Stone Cutter", new Produce(0, 1)));
+        }, new Produce(10, 10)));
+
+        group.appendChild(document.createElement('br'));
+
+        // Wheat
+        group.appendChild(createButton("Wheat", "3 Food", () => { this.AddToPile(new Produce(3)); }));
+        group.appendChild(createButton("Farmer", "0.8 Food per Tick", () => {
+            this.AddProducer(new Producer("Farmer", new Produce(0.8)));
+        }, new Produce(20, 20)));
     }
 
     UpdateStats() {
@@ -74,12 +109,13 @@ class IdleGame {
 
     Save() {
         let json = {
+            all_time: this.all_time.toJSON(),
             stockpile: this.stockpile.toJSON(),
             growth: this.growth.toJSON(),
 
-            resources: this.resources.map((res) => { return res.toJSON() }),
-            buildings: this.buildings.map((res) => { return res.toJSON() }),
-            producers: this.producers.map((res) => { return res.toJSON() })
+            resources: Array.from(this.resources, ([name, value]) => (value.toJSON())),
+            buildings: Array.from(this.buildings, ([name, value]) => (value.toJSON())),
+            producers: Array.from(this.producers, ([name, value]) => (value.toJSON())),
         }
 
         window.localStorage.setItem('game', JSON.stringify(json));
@@ -89,16 +125,17 @@ class IdleGame {
     }
 
     Load() {
-        let json = JSON.parse(window.localStorage.getItem('game') ?? {});
+        let json = JSON.parse(window.localStorage.getItem('game') ?? '{}');
 
+        this.all_time = new Produce(json.all_time);
         this.stockpile = new Produce(json.stockpile);
         this.growth = new Produce(json.growth); // might calculate on load
         
-        this.resources = json.resources?.map((res) => new Resource(res.name, new Produce(res.produce))) ?? [];
-        this.buildings = json.buildings?.map((res) => new Building(res.name, new Produce(res.produce))) ?? [];
-        this.producers = json.producers?.map((res) => new Producer(res.name, new Produce(res.produce))) ?? [];
+        this.resources = new Map(json.resources?.map((obj) => [obj.name, new Resource(obj)]) ?? []);
+        this.buildings = new Map(json.buildings?.map((obj) => [obj.name, new Building(obj)]) ?? []);
+        this.producers = new Map(json.producers?.map((obj) => [obj.name, new Producer(obj)]) ?? []);
 
-        console.log(this, json);
+        console.log(this, json, this.producers);
     }
 }
 
@@ -109,6 +146,7 @@ class Produce {
     science = 0;
     culture = 0;
 
+    // Can Accept food 
     constructor(food, work, gold, science, culture) {
         this.food = Number(food) || Number(food?.food) || 0;
         this.work = Number(work) || Number(food?.work) || 0;
@@ -139,6 +177,19 @@ class Produce {
         this.culture += produce.culture ?? 0;
     }
 
+    remove(produce) {
+        this.food -= produce.food ?? 0;
+        this.work -= produce.work ?? 0;
+        this.gold -= produce.gold ?? 0;
+        this.science -= produce.science ?? 0;
+        this.culture -= produce.culture ?? 0;
+    }
+
+    has(produce) {
+        return this.food >= produce.food && this.work >= produce.work && this.gold >= produce.gold
+            && this.science >= produce.science && this.culture >= produce.culture;
+    }
+
     toJSON() {
         return {
             food: this.food,
@@ -152,6 +203,7 @@ class Produce {
 
 class Resource {
     name = 'Resource';
+    count = 1;
     produce;
 
     constructor() {}
@@ -161,6 +213,7 @@ class Resource {
 
 class Building {
     name = 'Building';
+    count = 1;
     produce;
 
     constructor() {}
@@ -170,16 +223,30 @@ class Building {
 
 class Producer {
     name = "Producer";
+    count = 1;    
     produce;
 
-    constructor(name, perTick = new Produce()) {
-        this.name = name || "Producer";
-        this.produce = perTick;
+    // Can Accept Name parameter as Procder toJSON output
+    constructor(name, perTick = new Produce(), count = 1) {
+        this.name = name?.name || name || "Producer";
+        this.count = name?.count || count;
+        this.produce = name?.produce ? name?.produce?.toJSON ? name.produce : new Produce(name.produce) : perTick;
+    }
+
+    asCount(produce = this.count) {
+        return {
+            food: this.produce.food * produce,
+            work: this.produce.work * produce,
+            gold: this.produce.gold * produce,
+            science: this.produce.science * produce,
+            culture: this.produce.culture * produce,
+        }
     }
 
     toJSON() {
         return {
             name: this.name,
+            count: this.count,
             produce: this.produce?.toJSON()
         }
     }
